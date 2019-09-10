23d0ee92412f98754fdd0b4888698ddede400f4b
MarsVoyag <m.rajoygusta@mailcat.cat>
2019-09-10 16:36:32 +0200

    Primer commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 MF0493_3
+
+Aplicació Android entregable de la unitat Formativa
+
+Per Sergio Martínez Leonora
---
+++ app/src/main/java/com/example/sergiomar_mf04933/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sergiomar_mf04933;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
