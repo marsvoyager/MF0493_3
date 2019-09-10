672f8492486a4eaf902273cddc0fb1d5d82d6074
sermar <m.rajoygusta@mailcat.cat>
2019-09-10 16:46:38 +0200

    Segundo commit desde Stud

--- app/src/main/java/com/example/sergiomar_mf04933/MainActivity.java
+++ app/src/main/java/com/example/sergiomar_mf04933/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.sergiomar_mf04933;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        prefs = getSharedPreferences("PreferenciasApp", Context.MODE_PRIVATE);
+        SharedPreferences.Editor shpred =  prefs.edit();
+
+        //shpred.putInt();
+        //shpred.commit()
+        //prefs.getInt()
+
+
+
     }
 }
